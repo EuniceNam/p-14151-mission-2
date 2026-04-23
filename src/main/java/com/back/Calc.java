e807456bf2ebe01d2f1a09694d82ceb38b6dc907
EuniceNam <caellime@gmail.com>
2026-04-24 00:20:42 +0900

    refactor: t20 blue

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -1,42 +1,91 @@
 package com.back;
 
+import java.util.Arrays;
+
 public class Calc {
-    static final String ADD = "+";
-    static final String SUB = "-";
-    static final String MULT = "*";
 
     public static int run(String s0) {
-        String s = s0.replaceAll("\\(+", "(").replaceAll("\\)+", ")");
-        int lpidx = s.indexOf("(");
-        int rpidx;
-        if(lpidx != -1) {
-            rpidx = s.indexOf(")");
-            return calc(s.substring(lpidx+1, rpidx)); //(안의 exp만)
-        } else {
-            return calc(s);
-        }
+        String s = s0.replaceAll("\\(","( " ).replaceAll("\\)", " )");
+        String[] items = s.split(" ");
+
+        return calc(items);
     }
 
-    private static int calc(String s) {
-        String[] items = s.split(" ");
-        int len = items.length;
-        for(int i=1; i<=len/2; i++) {
-            if (items[i*2-1].equals(MULT)) {
-                items[i*2] = String.valueOf(Integer.parseInt(items[i*2-2]) * Integer.parseInt(items[i*2]));
-                items[i*2-1] = "+";
-                items[i*2-2] = "0";
-            }
-        }
-        int lop = Integer.parseInt(items[0]);
-        int rop;
-        for(int i=1; i<=len/2; i++) {
-            rop = Integer.parseInt(items[i * 2]);
-            switch (items[i*2-1]) {
-                case ADD -> lop += rop;
-                case SUB -> lop -= rop;
-                default -> {}
+    private static int calc(String[] items) {
+        int lop = nextInt(items);
+        String[] nextExp = null;
+        for (int i = 0; i < items.length; i++) {
+            Op o = Op.from(items[i]);
+            if (o != null) {
+                nextExp = Arrays.copyOfRange(items, i + 1, items.length);
+                switch (o) {
+                    case ADD -> {
+                        if (Op.ADD.priorLv() >= nexOpPLv(nextExp)) {
+                            lop += nextInt(nextExp);
+                        } else {
+                            return lop + calc(nextExp);
+                        }
+                    }
+                    case SUB -> {
+                        if (Op.SUB.priorLv() >= nexOpPLv(nextExp)) {
+                            lop -= nextInt(nextExp);
+                        } else {
+                            return lop - calc(nextExp);
+                        }
+                    }
+                    case MULT -> {
+                        if (Op.MULT.priorLv() >= nexOpPLv(nextExp)) {
+                            lop *= nextInt(nextExp);
+                        } else {
+                            return lop * calc(nextExp);
+                        }
+                    }
+                    case LP -> { return calc(nextExp);}
+                    case RP -> { /*return lop;*/}
+                    default -> {} // number
+                }
             }
         }
         return lop;
+    }
+
+    private static int nextInt(String[] exp) {
+        for(String s: exp) {
+            if(s.matches("-?\\d+")) {
+                return Integer.parseInt(s);
+            }
+        }
+        return Integer.MIN_VALUE;
+    }
+
+    private static int nexOpPLv(String[] exp) {
+        for(String s: exp) {
+            Op o = Op.from(s);
+            if (o != null) { return o.priorLv();}
+        }
+        return 0; // exp의 끝
+    }
+
+    private enum Op {
+        ADD("+"),SUB("-"), MULT ("*"), LP("("), RP(")");
+
+        private final String symbol;
+        Op(String symbol) { this.symbol = symbol;}
+
+        public static Op from(String s) {
+            for (Op o: values()) {
+                if (o.symbol.equals(s)) { return o;}
+            }
+            return null;
+        }
+        // priorLv - 시작, ): 0, +|-: 1, *: 2, (: 3
+        public int priorLv() {
+            return switch (this) {
+                case ADD, SUB -> 1;
+                case MULT -> 2;
+                case LP -> 3;
+                case RP -> 0;
+            };
+        }
     }
 }
